4a6ae2a8e08585e5431cd71d0fb0204aef714555
Edward <krisandjohn42@hotmail.com>
2018-03-20 18:32:49 -0500

    solved problem

--- src/Program.java
+++ src/Program.java
@@ -7,6 +7,19 @@ public class Program
 	{
 		//Create a new Store object and figure out the most expensive item in the 
 		//stores Contents, wherever that may be...
+		Store IDK= new Store();
+		System.out.println(IDK.Heater.Value);
+		System.out.println("bag worth = " + IDK.Merchant.Bag.cost);
+		//System.out.println(IDK.Merchant.Bag.Contents);
+		Item i  = new Item();
 		
+		for(Item bagStuff : IDK.Merchant.Bag.Contents)
+		{
+			System.out.println(bagStuff.Name + " " + bagStuff.Value);
+			if (bagStuff.Value > i.Value)
+				i = bagStuff;
+		}
+		
+		System.out.println("AND THE MOST VALUABLE THING IN THE BAG IS " + i.Name + " GOING FOR " + i.Value);
 	}
 }
